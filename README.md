c5454bc9a86e7e0f8fe2a8f31744715adab7d059
imkarimkarim <imkarimkarim@pm.me>
2023-06-10 18:14:50 +0330

    in the name of internet

---
+++ README.md
@@ -0,0 +1,18 @@
+# tldp | تست لینک درگاه پرداخت
+
+نرم افزار آزاد برای تست معتبر بودن درگاه های پرداخت بانکی آنلاین و جلوگیری از فیشنگ
+
+## Getting Started
+
+1. clone this repo
+
+2. install and run
+
+```bash
+npm install # pnpm install / yarn
+npm run dev # pnpm dev / yarn dev
+```
+
+## Contribution
+
+contributions are welcomed 🌻
---
+++ script.js
@@ -0,0 +1,119 @@
+function isValidUrl(str) {
+    let url;
+    try {
+        url = new URL(str);
+    } catch (_) {
+        return false;
+    }
+    return url.protocol === 'http:' || url.protocol === 'https:';
+}
+
+function isHttps(str) {
+    let first8Chars = '';
+    for (let i = 0; i < 8; i++) {
+        first8Chars += str[i];
+    }
+    return first8Chars === 'https://';
+}
+
+const gateways = [
+    'asan.shaparak.ir',
+    'bpm.shaparak.ir',
+    'pec.shaparak.ir',
+    'pecco.shaparak.ir',
+    'sep.shaparak.ir',
+    'sep2.shaparak.ir',
+    'pep.shaparak.ir',
+    'pna.shaparak.ir',
+    'sadad.shaparak.ir',
+    'ikc.shaparak.ir',
+    'fanava.shaparak.ir',
+    'fcp.shaparak.ir',
+    'mabna.shaparak.ir',
+    'ecd.shaparak.ir',
+    'pas.shaparak.ir',
+    'sepehr.shaparak.ir',
+    'say.shaparak.ir',
+];
+
+const companies = [
+    'آسان پرداخت پرشین',
+    'به پرداخت ملت',
+    'تجارت الکترونیک پارسیان',
+    'تجارت الکترونیک پارسیان',
+    'پرداخت الکترونیک سامان',
+    'پرداخت الکترونیک سامان',
+    'پرداخت الکترونیک پاسارگاد',
+    'پرداخت نوین آرین',
+    'پرداخت الکترونیک سداد',
+    'کارت اعتباری ایران کیش',
+    'فن آوا کارت',
+    'فن آوا کارت',
+    'پرداخت الکترونیک سپهر',
+    'الکترونیک کارت دماوند',
+    'سایان کارت',
+    'پرداخت الکترونیک سپهر',
+    'سایان کارت',
+];
+
+function isValidPaymentGateway(str, callback) {
+    const url = new URL(str);
+
+    let theCompanyName;
+    if (gateways.includes(url.hostname)) {
+        const gate = gateways.findIndex((g) => g === url.hostname);
+        theCompanyName = companies[gate];
+        if (typeof callback === 'function') {
+            callback(true, theCompanyName);
+        }
+    } else {
+        callback(false);
+    }
+}
+
+const infoEl = document.querySelector('.info');
+const errorEl = document.querySelector('.error');
+const successEl = document.querySelector('.success');
+
+let errors = [],
+    infos = [],
+    link;
+
+const checkLink = (e) => {
+    e.preventDefault();
+
+    errorEl.innerHTML = '';
+    infoEl.innerHTML = '';
+    successEl.innerHTML = '';
+
+    errors = [];
+    infos = [];
+    link = document.querySelector('input').value.trim();
+
+    if (link.length === 0) {
+        infos.push('لطفا لینک را وارد کنید...');
+    } else if (!isValidUrl(link)) {
+        infos.push('متن وارد شده لینک نیست!');
+    } else if (!isHttps(link)) {
+        errors.push('خطر!!! لینک وارد شده از پروتکل https استفاده نمیکند!');
+    } else {
+        isValidPaymentGateway(link, (valid, company) => {
+            if (!valid) {
+                errors.push('خطر!!! لینک وارد شده در لیست درگاه های معتبر یافت نشد!');
+            } else {
+                successEl.innerHTML = `لینک معتبر و متعلق به شرکت "${company}" است.`;
+            }
+        });
+    }
+
+    if (errors.length > 0) {
+        errorEl.innerHTML = errors[0];
+    }
+
+    if (infos.length > 0) {
+        infoEl.textContent = infos[0];
+    }
+};
+
+document.getElementById('form').addEventListener('submit', checkLink);
+document.getElementById('link-input').addEventListener('input', checkLink);
